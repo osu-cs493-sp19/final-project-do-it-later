e1e20ad9e35cd0477f1486a9b4caf21eae170059
Phi Luu <philectron@users.noreply.github.com>
2019-06-11 20:21:43 -0700

    Improve GET /courses to meet requirements

--- api/courses.js
+++ api/courses.js
@@ -5,32 +5,44 @@
 const router = require('express').Router();
 
 const {
-  getCoursePage
+  CourseSchema,
+  getCoursePage,
 } = require('../models/course');
 
 /*
  * GET /courses
  * GET /courses?page=3
  *
- * Fetches a paginated list of all Courses.
- * The URL parameter  page  is optional; if not specified, displays page 1.
+ * Fetches the paginated list of all Courses. The Courses returned do not
+ * contain the list of Students in the Course or the Assignments for the Course.
  */
 router.get('/', async (req, res, next) => {
   try {
+    // collect URL parameters
     const currentPage = parseInt(req.query.page) || 1;
+    const subject = req.query.subject || '';
+    const number = req.query.number || '';
+    const term = req.query.term || '';
 
     // fetch current page's info and generate HATEOAS links of surrounding pages
-    const coursePage = await getCoursePage(currentPage);
+    const coursePage = await getCoursePage(currentPage, subject, number, term);
     coursePage.links = {};
+    const subjectQuery = subject ? `&subject=${subject}` : '';
+    const numberQuery = number ? `&number=${number}` : '';
+    const termQuery = term ? `&term=${term}` : '';
     // if not last page, provide links to next and last pages
     if (coursePage.currentPage < coursePage.totalPages) {
-      coursePage.links.nextPage = `/courses?page=${coursePage.currentPage + 1}`;
-      coursePage.links.lastPage = `/courses?page=${coursePage.totalPages}`;
+      coursePage.links.nextPage = `/courses?page=${coursePage.currentPage + 1}` +
+          subjectQuery + numberQuery + termQuery;
+      coursePage.links.lastPage = `/courses?page=${coursePage.totalPages}` +
+          subjectQuery + numberQuery + termQuery;
     }
     // if not first page, provide links to previous and first pages
     if (coursePage.currentPage > 1) {
-      coursePage.links.prevPage = `/courses?page=${coursePage.currentPage - 1}`;
-      coursePage.links.firstPage = `/courses?page=1`;
+      coursePage.links.prevPage = `/courses?page=${coursePage.currentPage - 1}` +
+          subjectQuery + numberQuery + termQuery;
+      coursePage.links.firstPage = `/courses?page=1` +
+          subjectQuery + numberQuery + termQuery;
     }
 
     res.status(200).send(coursePage);
--- models/course.js
+++ models/course.js
@@ -8,72 +8,81 @@ const mysqlPool = require('../lib/mysqlPool');
  * This schema is used for validating the content of the request body before
  * inserting it to the database.
  */
-const courseSchema = {
+const CourseSchema = {
   subject: { required: true },
   number: { required: true },
   title: { required: true },
   term: { required: true },
   instructor_id: { required: true }
 };
-exports.courseSchema = courseSchema;
-
-/*
- * Returns a Promise that
- * - resolves to the number of existing courses on success, or
- * - rejects with an error on failure.
- */
-function countCourses() {
-  return new Promise((resolve, reject) => {
-    const mysqlQuery = 'SELECT COUNT(id) AS count FROM courses';
-    mysqlPool.query(mysqlQuery, (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(results[0].count);
-      }
-    });
-  });
-}
+exports.CourseSchema = CourseSchema;
 
 /*
  * Returns a Promise that
  * - resolves to an object containing info of a page of existing courses, or
  * - rejects with an error on failure.
+ *
+ * Params:
+ *   page     Page of Courses to fetch. Default to 1 if not specified.
+ *   subject  Fetch only Courses with the specified subject code.
+ *   number   Fetch only Courses with the specified course number.
+ *   term     Fetch only Courses with the specified academic term.
  */
-function getCoursePage(currentPage) {
-  return new Promise(async (resolve, reject) => {
-    const numCourses = await countCourses();
-    const pageSize = 10;  // courses per page
+function getCoursePage(page, subject, number, term) {
+  return new Promise((resolve, reject) => {
+    // filter Course's subject, number, and term if provided
+    // otherwise, `LIKE %%` will be used to match all patterns
+    const sql =
+      `SELECT *
+      FROM (
+        SELECT *
+        FROM courses
+        WHERE
+          LOWER(subject) ${subject ? '= ?' : 'LIKE ?'}
+          AND LOWER(number) ${number ? '= ?' : 'LIKE ?'}
+          AND LOWER(term) ${term ? '= ?' : 'LIKE ?'}
+      ) AS t
+      ORDER BY id ASC`;
+    mysqlPool.query(
+      sql,
+      [
+        subject ? subject : '%%',
+        number ? number : '%%',
+        term ? term : '%%',
+      ],
+      (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          // pagination must come after filtering
+          const numCourses = results.length;
+          const pageSize = 10;  // Courses per page
 
-    // e.g. if 29 courses in total,
-    //   then page 1 (1-10), page 2 (11-20), page 3 (21-29)
-    // e.g. if 30 courses in total,
-    //   then page 1 (1-10), page 2 (11-20), page 3 (21-30)
-    // e.g. if 31 courses in total,
-    //   then page 1 (1-10), page 2 (11-20), page 3 (21-30), page 4 (31)
-    const lastPage = Math.ceil(numCourses / pageSize);
+          // e.g. if 31 Courses in total, then
+          // page 1 (1-10), page 2 (11-20), page 3 (21-30), page 4 (31)
+          const lastPage = Math.ceil(numCourses / pageSize);
 
-    // make sure the current page is within [1..lastPage]
-    currentPage = (currentPage > lastPage) ? lastPage : currentPage;
-    currentPage = (currentPage < 1) ? 1 : currentPage;
+          // force the current page to be within [1..lastPage]
+          page = (page > lastPage) ? lastPage : page;
+          page = (page < 1) ? 1 : page;
 
-    // e.g. if on page 2, the offset will be 10 courses
-    const offset = (currentPage - 1) * pageSize;
+          // e.g. if on page 2, the offset will be 10 courses
+          const offset = (page - 1) * pageSize;
 
-    const mysqlQuery = 'SELECT * FROM courses ORDER BY id LIMIT ?, ?';
-    mysqlPool.query(mysqlQuery, [offset, pageSize], (err, results) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve({
-          courses: results,
-          currentPage: currentPage,
-          totalPages: lastPage,
-          pageSize: pageSize,
-          totalCourses: numCourses
-        });
+          // array indices start at 0,
+          // so the page starts at offset and stops before offset + pageSize
+          results = results.slice(offset, offset + pageSize);
+
+          resolve({
+            courses: results,
+            currentPage: page,
+            totalPages: lastPage,
+            pageSize: pageSize,
+            totalCourses: numCourses
+          });
+        }
       }
-    });
+    );
   });
 }
 exports.getCoursePage = getCoursePage;
